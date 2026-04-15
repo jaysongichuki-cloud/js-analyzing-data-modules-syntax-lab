c4d05d1fa4df64eee529b89622eee69837b16880
Jayson <jayson.gichuki@student.moringaschool.com>
2026-04-15 19:48:05 +0300

    first commit

--- index.js
+++ index.js
@@ -4,3 +4,19 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require("datejs");
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(arr => {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  });
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
